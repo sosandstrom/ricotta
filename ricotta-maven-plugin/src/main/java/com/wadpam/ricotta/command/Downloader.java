cfa2de8c59db8e9637fb670e21853ffbb4191f48
os <devnull@localhost>
2011-01-11 14:09:14 +0700

    command-line should work

--- ricotta-maven-plugin/src/main/java/com/wadpam/ricotta/command/Downloader.java
+++ ricotta-maven-plugin/src/main/java/com/wadpam/ricotta/command/Downloader.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.StatusLine;
@@ -15,7 +16,7 @@ import org.apache.maven.plugin.logging.Log;
 
 public class Downloader {
 
-    private static Log LOG;
+    private static Log LOG = null;
 
     protected static void warn(String s) {
         if (null != LOG) {
@@ -31,6 +32,18 @@ public class Downloader {
 
     public static void download(String projectName, String languageCode, String templateName, File destination)
             throws ClientProtocolException, IOException {
+        File folder = destination.getParentFile();
+        if (false == folder.exists()) {
+            folder.mkdirs();
+        }
+        FileOutputStream fos = new FileOutputStream(destination);
+        PrintStream ps = new PrintStream(fos);
+        download(projectName, languageCode, templateName, ps);
+        ps.close();
+    }
+
+    public static void download(String projectName, String languageCode, String templateName, PrintStream fos)
+            throws ClientProtocolException, IOException {
         String url = "http://ricotta-ost.appspot.com/projects/" + projectName + "/languages/" + languageCode + "/templates/"
                 + templateName + '/';
         info("Download URL " + url);
@@ -44,11 +57,6 @@ public class Downloader {
         final StatusLine status = response.getStatusLine();
         if (status.getStatusCode() == 200) {
             InputStream is = response.getEntity().getContent();
-            File folder = destination.getParentFile();
-            if (false == folder.exists()) {
-                folder.mkdirs();
-            }
-            FileOutputStream fos = new FileOutputStream(destination);
 
             byte buf[] = new byte[1024];
             int count;
@@ -56,9 +64,8 @@ public class Downloader {
                 fos.write(buf, 0, count);
             }
 
-            fos.close();
             is.close();
-            info("Downloaded " + destination.getAbsolutePath());
+            info("Downloaded " + projectName + '/' + languageCode + '/' + templateName);
         }
         else {
             warn("HTTP " + status.getStatusCode() + " " + status.getReasonPhrase());
@@ -69,10 +76,11 @@ public class Downloader {
 
     /**
      * @param args
+     * @throws IOException
+     * @throws ClientProtocolException
      */
-    public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+    public static void main(String[] args) throws ClientProtocolException, IOException {
+        download(args[0], args[1], args[2], System.out);
     }
 
     public static void setLog(Log log) {
